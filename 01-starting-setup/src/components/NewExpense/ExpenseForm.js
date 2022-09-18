36bdb2da7aa4b91d57c1fc7706ee8bfd1c45848f
Nhahan <kisy324@naver.com>
2022-09-18 23:50:17 +0900

    Init ExpenseForm

---
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './ExpenseForm.css';
+
+const ExpenseForm = () => {
+    return <div className="expense-form">
+    <form></form>
+</div>
+};
+
+export default ExpenseForm;
