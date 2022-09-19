001b8208781cd805467f82a7b39f3e9c99b5856c
Nhahan <kisy324@naver.com>
2022-09-19 23:23:28 +0900

    Integrated useState using spread operator

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -3,15 +3,32 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
   };
 
-  const amountChangeHandler = (event) => {};
+  const amountChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
+  };
 
-  const dateChangeHandler = (event) => {};
+  const dateChangeHandler = (event) => {
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
+  };
 
   return (
     <form>
@@ -22,11 +39,21 @@ const ExpenseForm = () => {
         </div>
         <div className="newexpense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            onChange={amountChangeHandler()}
+            min="0.01"
+            step="0.01"
+          />
         </div>
         <div className="newexpense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            onChange={dateChangeHandler()}
+            min="2019-01-01"
+            max="2022-12-31"
+          />
         </div>
         <div className="new-expense_actions">
           <button type="submit">Add Expense</button>
