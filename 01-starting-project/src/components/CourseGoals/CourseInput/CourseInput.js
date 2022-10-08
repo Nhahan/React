9d80b5b60e9d6d92c54e57f734744871f204a573
Nhahan <kisy324@naver.com>
2022-10-08 20:41:14 +0900

    Add button css

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -6,12 +6,14 @@ import './CourseInput.css';
 
 const FormControl = styled.div`
   margin: 0.5rem 0;
+
   & label {
     font-weight: bold;
     display: block;
     margin-bottom: 0.5rem;
     color: ${props => (props.invalid ? 'red' : 'black')};
   }
+
   & input {
     display: block;
     width: 100%;
@@ -21,6 +23,7 @@ const FormControl = styled.div`
     line-height: 1.5rem;
     padding: 0 0.25rem;
   }
+  
   & input:focus {
     outline: none;
     background: #fad0ec;
